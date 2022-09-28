ab234d791010d34a8a3fbbffb15cbc3e2450ef59
d-takeuchi <kuronk.kuro3@gmail.com>
2022-09-28 23:09:11 +0900

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
 import { Layout } from '../components/Layout'
 import type { NextPage } from 'next'
+import { css } from '@emotion/react'
 
 const Home: NextPage = () => {
   return (
     <Layout title={'トップページ'}>
-      <p>Hello World</p>
+      <div css={[wrapper, homeContent]}>
+        <h2 css={pageTitle}>Hello World</h2>
+        <p>d-takeuchi's Engineer Blog</p>
+      </div>
     </Layout>
   )
 }
 
+const wrapper = css`
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 4%;
+`
+
+const homeContent = css`
+  text-align: center;
+  margin-top: 10%;
+  p {
+    font-size: 1.125rem;
+    margin: 10px 0 42px;
+  }
+`
+
+const pageTitle = css`
+  font-size: 3rem;
+`
 export default Home
