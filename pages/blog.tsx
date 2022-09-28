4781d4aedb4f0ee304b5a7c9b19e7205eafdc743
d-takeuchi <kuronk.kuro3@gmail.com>
2022-09-29 08:21:32 +0900

    wip

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,21 @@
 import { Layout } from '../components/Layout'
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
+import { client } from '../libs/client'
+import type { Blog as BlogType } from '../types/type'
 
-const Blog: NextPage = () => {
+interface Response {
+  contents: BlogType[]
+  totalCount: number
+  offset: number
+  limit: number
+}
+
+interface Props {
+  blog: BlogType[]
+}
+
+const Blog: NextPage<Props> = (props) => {
+  const { blog } = props
   return (
     <Layout title='ブログ'>
       <p>blog page</p>
@@ -9,4 +23,13 @@ const Blog: NextPage = () => {
   )
 }
 
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const blogResponse = await client.get<Response>({
+    endpoint: 'blog',
+  })
+  const blog = blogResponse.contents
+  return {
+    props: { blog },
+  }
+}
 export default Blog
