92710462163c6c245afa0729707e77a5674c50b0
d-takeuchi <kuronk.kuro3@gmail.com>
2022-10-01 23:50:26 +0900

    wip

--- __test__/Blog.test.tsx
+++ __test__/Blog.test.tsx
@@ -6,3 +6,36 @@ import { setupServer } from 'msw/node'
 import type { Blog } from '../types/type'
 
 initTestHelpers()
+
+const handlers = [
+  rest.get('/blog', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 1,
+          title: 'hoge',
+          body: 'aiueo',
+        },
+      ])
+    )
+  }),
+]
+
+const server = setupServer(...handlers)
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe(`ブログページ`, () => {
+  it('getStaticPropsによってブログ一覧情報がブログページに表示できているか', async () => {
+    expect(true).toBe(true)
+  })
+})
