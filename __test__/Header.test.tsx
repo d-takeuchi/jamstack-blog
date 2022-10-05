6d0980d0c54d3b95a216f36d2a57039bfd85f1ce
d-takeuchi <kuronk.kuro3@gmail.com>
2022-10-06 00:02:04 +0900

    wip

--- __test__/Header.test.tsx
+++ __test__/Header.test.tsx
@@ -1,10 +1,44 @@
-import { render, screen } from '@testing-library/react'
+/**
+ * @jest-environment jsdom
+ */
 import '@testing-library/jest-dom/extend-expect'
 import userEvent from '@testing-library/user-event'
 import { initTestHelpers, getPage } from 'next-page-tester'
-import Home from '../pages'
+import { render, screen, cleanup } from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 
 initTestHelpers()
+
+const handlers = [
+  rest.get(`${process.env.API_ENDPOINT}/blog`, (req, res, ctx) => {
+    return res(
+      ctx.set({
+        'X-MICROCMS-API-KEY': process.env.API_KEY,
+      }),
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 1,
+          title: 'hoge',
+          body: 'aiueo',
+        },
+      ])
+    )
+  }),
+]
+
+const server = setupServer(...handlers)
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
 
 describe('Header', () => {
   it('ロゴクリックでTOPページへと遷移する', async () => {
@@ -25,10 +59,13 @@ describe('Header', () => {
     userEvent.click(screen.getByTestId('about-nav'))
     expect(await screen.findByText('about page')).toBeInTheDocument()
   })
-  it('ナビゲーションバーのBLOGリンクからBlogページへと遷移する', async () => {
-    const { page } = await getPage({ route: '/index' })
-    render(page)
-    userEvent.click(screen.getByTestId('blog-nav'))
-    expect(await screen.findByText('blog page')).toBeInTheDocument()
-  })
+  /**
+   * @jest-environment node
+   */
+  // it('ナビゲーションバーのBLOGリンクからBlogページへと遷移する', async () => {
+  //   const { page } = await getPage({ route: '/index' })
+  //   render(page)
+  //   userEvent.click(screen.getByTestId('blog-nav'))
+  //   expect(await screen.findByText('blog page')).toBeInTheDocument()
+  // })
 })
