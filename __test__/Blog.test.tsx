075bb4e7ad8800a35f84ced5210f0b219a5e21f4
d-takeuchi <kuronk.kuro3@gmail.com>
2022-10-06 00:00:28 +0900

    wip

--- __test__/Blog.test.tsx
+++ __test__/Blog.test.tsx
@@ -1,27 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen, cleanup } from '@testing-library/react'
+import { cleanup, render, screen } from '@testing-library/react'
 import { rest } from 'msw'
 import { getPage, initTestHelpers } from 'next-page-tester'
 import { setupServer } from 'msw/node'
-import type { Blog } from '../types/type'
 
 initTestHelpers()
 
 const handlers = [
-  rest.get('/blog', (req, res, ctx) => {
+  rest.get(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/blog`, (req, res, ctx) => {
     return res(
+      ctx.set({
+        'X-MICROCMS-API-KEY': process.env.API_KEY,
+      }),
       ctx.status(200),
-      ctx.json([
-        {
-          id: 1,
-          title: 'hoge',
-          body: 'aiueo',
-        },
-      ])
+      ctx.json({
+        contents: [
+          {
+            id: 'fugafuga2',
+            createdAt: '2022-09-20T22:48:13.140Z',
+            updatedAt: '2022-09-20T22:48:13.140Z',
+            publishedAt: '2022-09-20T22:48:13.140Z',
+            revisedAt: '2022-09-20T22:48:13.140Z',
+            title: 'テストタイトル1',
+            body: '<h2 id="h48034554a9">こんにちは、初めてのブログです</h2><h3 id="h09e49f209e">Next.js + microCMSによるJamstackl構成のブログです</h3>',
+          },
+          {
+            id: 'hogehoge2',
+            createdAt: '2022-09-20T22:48:13.140Z',
+            updatedAt: '2022-09-20T22:48:13.140Z',
+            publishedAt: '2022-09-20T22:48:13.140Z',
+            revisedAt: '2022-09-20T22:48:13.140Z',
+            title: 'テストタイトル2',
+            body: '<h2 id="h48034554a9">こんにちは、初めてのブログです</h2><h3 id="h09e49f209e">Next.js + microCMSによるJamstackl構成のブログです</h3>',
+          },
+        ],
+      })
     )
   }),
 ]
-
 const server = setupServer(...handlers)
 beforeAll(() => {
   server.listen()
@@ -36,6 +55,11 @@ afterAll(() => {
 
 describe(`ブログページ`, () => {
   it('getStaticPropsによってブログ一覧情報がブログページに表示できているか', async () => {
-    expect(true).toBe(true)
+    const { page } = await getPage({
+      route: '/blog',
+    })
+    render(page)
+    expect(await screen.findByText('テストタイトル1')).toBeInTheDocument()
+    expect(await screen.findByText('テストタイトル2')).toBeInTheDocument()
   })
 })
